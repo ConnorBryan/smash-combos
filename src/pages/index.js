d2421cba90874671c8d9037d8b88319765ba36ad
Connor Bryan <cchromium@gmail.com>
2018-12-22 15:27:22 -0600

    Add SCSS capability

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
+import './master.scss'
 
 const IndexPage = () => (
-  <Layout>
+  <section className="Master">
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
@@ -15,7 +15,7 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  </section>
 )
 
 export default IndexPage
